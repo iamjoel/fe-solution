a1d1532f777ac0f4aaabffae58effe86919830dc
金伟强 <jinweiqiang@coding.net>
2021-02-24 18:07:35 +0800

    add code

--- src/effective/quick-find-comp-src/react/react-dev-inspector/src/App.js
+++ src/effective/quick-find-comp-src/react/react-dev-inspector/src/App.js
@@ -1,24 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Inspector } from 'react-dev-inspector'
+
+const InspectorWrapper = process.env.NODE_ENV === 'development'
+  ? Inspector
+  : React.Fragment
+
+console.log(process.env.NODE_ENV)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InspectorWrapper
+      // props docs see:
+      // https://github.com/zthxxx/react-dev-inspector#inspector-component-props
+      keys={['control', 'b']}
+      disableLaunchEditor={false}
+      onHoverElement={(params) => {}}
+      onClickElement={(params) => {}}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            按 ctrl + b 来开启, 点击调整到源码。
+          </p>
+        </header>
+      </div>
+    </InspectorWrapper>
   );
 }
 

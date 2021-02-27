49802d95f6a5da520e9ea23a17efd0eeb1837ef0
九彩拼盘 <iamjoel007@gmail.com>
2021-02-27 23:37:38 +0800

    feat: dnd upload file use vue (#13)
    * feat: avoid-css-conflict
    
    * feat: large list
    
    * feat: dnd-sort
    
    * add file
    
    * 拖拽上传文件
    
    文件放到区域内，实现上传效果
    
    * add react dnd upload file
    
    * add des
    
    * feat: dnd upload file use react
    
    * feat: quick find comp src
    
    * 优化文件上传
    
    1，对UI的优化
    2，对业务逻辑的优化
    3，对css起名的优化
    
    * 优化文件上传代码
    
    1，组件引用名大写
    2，优化代码，把filter改为map写法
    
    * feat: async data in order render
    
    * feat: quick gen code
    
    Co-authored-by: 金伟强 <jinweiqiang@coding.net>
    Co-authored-by: zhongmin_ext <zhongmin_ext@cop.cmsoft.com.cn>

---
+++ src/scene/dnd-upload-file/vue/src/components/upLoad.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="dnd-upload">
+    <div class="dnd-upload__area" :class="{'dnd-upload__area--over': isOver}" @dragover="handleDragOver($event)"
+      @dragleave="handleDragLeave($event)" @drop="drop">
+      请将文件拖到这里(支持同时拖多个)
+      <div>一堆其他内容</div>
+      <div>一堆其他内容</div>
+      <div>一堆其他内容</div>
+    </div>
+    <div class="dnd-upload__uploaded">
+      <h2>已上传文件</h2>
+      <div class="dnd-upload__uploaded-list">
+        <div
+          class="dnd-upload__uploaded-item"
+          v-for="(item, i) in uploadList"
+          :key="i"
+        >
+          <div>文件名: {{item.name}}</div>
+          <div class="dnd-upload__uploaded-item-size">大小: {{item.size}} KB</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      uploadList: [],
+      isOver: false
+    };
+  },
+  methods: {
+    handleDragOver(el){
+      this.isOver = true
+      el.preventDefault()
+      el.stopPropagation()
+    },
+    handleDragLeave(el){
+      this.isOver = false
+      el.preventDefault()
+      el.stopPropagation()
+    },
+    drop(el){
+      this.isOver = false
+      el.preventDefault()
+      el.stopPropagation()
+      let uploadFile = [...(el.dataTransfer.files)].map(item=>({
+        name: item.name,
+        size: item.size
+      }))
+      this.uploadList = [...this.uploadList, ...uploadFile]
+      console.log(this.uploadList)
+    },
+  },
+};
+</script>
+<style scoped>
+.dnd-upload {
+  margin: 10px auto;
+  width: 500px;
+}
+
+.dnd-upload__area {
+  margin-bottom: 20px;
+  padding-top: 20px;
+  height: 200px;
+  text-align: center;
+  border: 3px dashed blue;
+}
+
+.dnd-upload__area--over {
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.dnd-upload__uploaded-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.dnd-upload__uploaded-item {
+  margin-right: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  box-shadow: 0 3px 8px 0 rgb(0 0 0 / 8%), 0 1px 2px 0 rgb(0 0 0 / 8%);
+  border-radius: 3px;
+}
+.dnd-upload__uploaded-item-size{
+  text-align:left;
+}
+</style>

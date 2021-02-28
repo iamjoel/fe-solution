f6a6e3df7d34f20c0bf9059ff983bfc892d0883f
zhongmin_ext <zhongmin_ext@cop.cmsoft.com.cn>
2021-02-28 22:48:28 +0800

    复制到剪切板上
    当点击复制按钮时，把内容复制到浏览器剪切板上

---
+++ src/scene/dnd-copy/vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="App">
+    <Copy></Copy>
+  </div>
+</template>
+
+<script>
+import Copy from './components/copy.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Copy
+  }
+}
+</script>
+
+<style>
+html,body{
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+#App {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/scene/dnd-copy/vue/src/components/copy.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="dnd-copy">
+    <div class="dnd-copy__content-copy">
+        我是要复制的内容
+      </div>
+    <button class="dnd-copy__btn" @click="copy">复制</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Copy",
+  data() {
+    return {};
+  },
+  methods: {
+    copy() {
+      let value = document.getElementsByClassName('dnd-copy__content-copy')[0].innerHTML;
+      this.copyValue(value)
+    },
+    copyValue(val){
+      var temp = document.createElement('textarea');
+      temp.value = val;
+      document.body.appendChild(temp);
+      temp.select(); // 选择对象
+      document.execCommand("Copy"); // 执行浏览器复制命令
+      console.log('复制成功');
+    }
+  },
+}
+</script>
+
+<style scoped>
+.dnd-copy {
+  padding: 20px;
+  height: 100%;
+  width: 100%;
+}
+.dnd-copy__content-copy {
+  border: 1px #ccc solid;
+  border-radius: 6px;
+  text-align: left;
+  padding: 10px 20px;
+}
+.dnd-copy__btn {
+  margin: 20px 10px;
+  padding: 5px 10px;
+  border: 1px #ccc solid;
+  border-radius: 3px;
+}
+</style>

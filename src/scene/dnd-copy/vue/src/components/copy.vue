76801b7e4c4fe5c714e882be80ee1aa1dda011c2
zhongmin_ext <zhongmin_ext@cop.cmsoft.com.cn>
2021-03-07 13:57:12 +0800

    完善clipboard浏览器不兼容处理
    完善clipboard浏览器不兼容处理

--- src/scene/dnd-copy/vue/src/components/copy.vue
+++ src/scene/dnd-copy/vue/src/components/copy.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="clipborad">
-    <div class="clipborad__content-copy">
-        我是要复制的内容
+    <div class="clipborad__content-copy" id="foo" :v-model="message">
+        {{message}}
       </div>
-    <button class="clipborad__btn" @click="handleCopy">复制</button>
+    <button class="clipborad__btn btn" data-clipboard-target="#foo" @click="handleCopy">复制</button>
   </div>
 </template>
 
 <script>
+import Clipboard from 'clipboard';
 export default {
   name: "Copy",
   data() {
-    return {};
+    return {
+      message: '我是要复制的内容46345678'
+    };
   },
   methods: {
     handleCopy() {
-      let value = document.getElementsByClassName('clipborad__content-copy')[0].innerHTML;
-      this.copyValue(value)
-    },
-    copyValue(val){
+      let support = !!document.queryCommandSupported;
+      support = support && !!document.queryCommandSupported('copy');
+      if(support){
       var temp = document.createElement('textarea');
-      temp.value = val;
+      temp.value = this.message;
       document.body.appendChild(temp);
       temp.select(); // 选择对象
       document.execCommand("Copy"); // 执行浏览器复制命令
       console.log('复制成功');
+      }else{
+        this.copyValue()
+      }
+    },
+    copyValue(){
+      var clipboard1 = new Clipboard('.btn');
+
+      clipboard1.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        e.clearSelection();
+      });
+
+      clipboard1.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+      });
     }
   },
 }

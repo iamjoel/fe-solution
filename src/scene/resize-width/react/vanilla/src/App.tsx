dfe46763e875b943439df1a8cbd3a8470140f947
金伟强 <jinweiqiang@coding.net>
2021-06-22 23:57:43 +0800

    article

--- src/scene/resize-width/react/vanilla/src/App.tsx
+++ src/scene/resize-width/react/vanilla/src/App.tsx
@@ -1,32 +1,32 @@
-import {useState, useCallback} from 'react';
+import {useState, useCallback} from 'react'
 import ResizeWidth from './components/resize-width/index.tsx'
-import s from './App.module.scss';
+import s from './App.module.scss'
 
-const MENU_MIN = 200;
-const MENU_MAX = 600;
-const MENU_DEFAULT = 300;
+const MENU_MIN = 200
+const MENU_MAX = 600
+const MENU_DEFAULT = 300
 function App() {
-  const storedMenuWidth = localStorage.getItem('app-left-menu-width');
-  const calculatedMenuWidth = storedMenuWidth ? parseInt(storedMenuWidth, 10) : MENU_DEFAULT;
-  const [menuWidth, setMenuWidth] = useState(calculatedMenuWidth);
-  const [expandLeftWidth, setExpandLeftWidth] = useState(calculatedMenuWidth);
+  const storedMenuWidth = localStorage.getItem('app-left-menu-width')
+  const calculatedMenuWidth = storedMenuWidth ? parseInt(storedMenuWidth, 10) : MENU_DEFAULT
+  const [menuWidth, setMenuWidth] = useState(calculatedMenuWidth)
+  const [expandLeftWidth, setExpandLeftWidth] = useState(calculatedMenuWidth)
  
   const handleResize = useCallback(offset => {
-    let res = menuWidth + offset;
+    let res = menuWidth + offset
     if (res < MENU_MIN) {
-      res = MENU_MIN;
+      res = MENU_MIN
     }
     if (res > MENU_MAX) {
-      res = MENU_MAX;
+      res = MENU_MAX
     }
-    setMenuWidth(res);
-    setExpandLeftWidth(res);
-    localStorage.setItem('app-left-menu-width', `${res}`);
-  }, [menuWidth]);
+    setMenuWidth(res)
+    setExpandLeftWidth(res)
+    localStorage.setItem('app-left-menu-width', `${res}`)
+  }, [menuWidth])
 
   const handleToggleExpand = useCallback((isExpend) => {
-    setMenuWidth(isExpend ? expandLeftWidth : 0);
-  }, [expandLeftWidth]);
+    setMenuWidth(isExpend ? expandLeftWidth : 0)
+  }, [expandLeftWidth])
 
   return (
     <div
@@ -36,17 +36,15 @@ function App() {
         className={s.menu}
         style={{ width: `${menuWidth}px` }}
       >
-        菜单
         <ResizeWidth
           onResize={handleResize}
           onToggleExpand={handleToggleExpand}
         />
       </div>
       <div className={s.main}>
-        主体
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/scene/resize-width/react/vanilla/src/components/resize-width/index.tsx
+++ src/scene/resize-width/react/vanilla/src/components/resize-width/index.tsx
@@ -1,71 +1,71 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
-import { useDebounceFn } from 'ahooks';
-import cn from 'classnames';
-import s from './style.module.scss';
+import React, { FC, useCallback, useEffect, useState } from 'react'
+import { useDebounceFn } from 'ahooks'
+import cn from 'classnames'
+import s from './style.module.scss'
 
 export interface IResizeWidthProps {
-  onResize: (offset: number) => void;
-  onToggleExpand: (isExpand: boolean) => void;
+  onResize: (offset: number) => void
+  onToggleExpand: (isExpand: boolean) => void
 }
 
 const ResizeWidth: FC<IResizeWidthProps> = ({
   onToggleExpand,
   onResize
 }) => {
-  const [clientX, setClientX] = useState(0);
-  const [isResizing, setIsResizing] = useState(false);
-  const [prevUserSelectStyle, setPrevUserSelectStyle] = useState(getComputedStyle(document.body).userSelect);
+  const [clientX, setClientX] = useState(0)
+  const [isResizing, setIsResizing] = useState(false)
+  const [prevUserSelectStyle, setPrevUserSelectStyle] = useState(getComputedStyle(document.body).userSelect)
 
-  const handleStartResizing = useCallback((e: React.MouseEvent<HTMLElement>) => {
+  const handleStartResize = useCallback((e: React.MouseEvent<HTMLElement>) => {
     setClientX(e.clientX)
     setIsResizing(true)
     setPrevUserSelectStyle(getComputedStyle(document.body).userSelect)
     document.body.style.userSelect = 'none'
-  }, []);
+  }, [])
 
-  const handleStopResizing = useCallback(() => {
+  const handleStopResize = useCallback(() => {
     setIsResizing(false)
-    document.body.style.userSelect = prevUserSelectStyle;
-  }, [prevUserSelectStyle]);
+    document.body.style.userSelect = prevUserSelectStyle
+  }, [prevUserSelectStyle])
 
   const { run: didHandleResize } = useDebounceFn((e) => {
     if(!isResizing) {
-      return;
+      return
     }
-    const offset = e.clientX - clientX;
-    setClientX(e.clientX);
+    const offset = e.clientX - clientX
+    setClientX(e.clientX)
     onResize(offset)
   }, {
     wait: 0,
-  });
+  })
 
   const handleResize = useCallback(didHandleResize, [isResizing, clientX, didHandleResize])
 
   useEffect(() => {
-    document.addEventListener('mouseup', handleStopResizing)
+    document.addEventListener('mouseup', handleStopResize)
     document.addEventListener('mousemove', handleResize)
     return () => {
-      document.removeEventListener('mouseup', handleStopResizing)
+      document.removeEventListener('mouseup', handleStopResize)
       document.removeEventListener('mousemove', handleResize)
     }
-  }, [handleStopResizing, handleResize])
+  }, [handleStopResize, handleResize])
 
 
-  const [isExpand, setIsExpand] = useState(true);
+  const [isExpand, setIsExpand] = useState(true)
   const handleToggleExpand = () => {
-    const next = !isExpand;
-    onToggleExpand(next);
-    setIsExpand(next);
-  };
+    const next = !isExpand
+    onToggleExpand(next)
+    setIsExpand(next)
+  }
 
   return (
     <div
-      className={s.wrap}
-      onMouseDown={handleStartResizing}
+      className={s.resizeHandle}
+      onMouseDown={handleStartResize}
     >
       <div className={cn(s.toggleBtn, !isExpand && s.fold)} onClick={handleToggleExpand} />
       {isResizing && <div className={s.mask} />}
     </div>
-  );
-};
-export default ResizeWidth;
+  )
+}
+export default React.memo(ResizeWidth)

625e56168d0439325542f9f31a505db99716b11e
金伟强 <jinweiqiang@coding.net>
2022-05-05 17:59:42 +0800

    just add

---
+++ src/scene/admin/vue3-admin/src/components/ui/form/Form.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  [key: string]: any,
+}>()
+</script>
+
+<template>
+  <a-form v-bind="props">
+    <slot />
+  </a-form>
+</template>
---
+++ src/scene/admin/vue3-admin/src/components/ui/form/FormItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  [key: string]: any,
+}>()
+</script>
+
+<template>
+  <a-form-item v-bind="props">
+    <slot />
+  </a-form-item>
+</template>

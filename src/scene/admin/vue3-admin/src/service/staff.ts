2fdb42f6f0f5572bcecfab877e55ee5b34405b07
金伟强 <jinweiqiang@coding.net>
2022-05-13 16:39:57 +0800

    list page doc

--- src/scene/admin/vue3-admin/src/service/staff.ts
+++ src/scene/admin/vue3-admin/src/service/staff.ts
@@ -4,7 +4,7 @@ import type { sortType } from '@/define/list.d'
 import './staff.mock' // mock接口数据
 
 export const fetchList = (current: number, searchQuery: Record<string, any>, sortParams?: {key: string, value: sortType}) => {
-  return http.get('/api/pet/list', {
+  return http.get('/pet/list', {
     params: {
       current,
       q: JSON.stringify(searchQuery),
@@ -15,14 +15,14 @@ export const fetchList = (current: number, searchQuery: Record<string, any>, sor
 
 export const edit = (id: number, data: Record<string, any>) => {
   console.log(`edit: ${JSON.stringify(data)}`)
-  return http.patch(`/api/pet/${id}`, data)
+  return http.patch(`/pet/${id}`, data)
 }
 
 export const create = (data: Record<string, any>) => {
   console.log(`create: ${JSON.stringify(data)}`)
-  return http.put('/api/pet', data)
+  return http.put('/pet', data)
 }
 
 export const remove = (id: number, data: Record<string, any>) => {
-  return http.delete(`/api/pet/${id}`, data)
+  return http.delete(`/pet/${id}`, data)
 }
--- src/scene/admin/vue3-admin/src/utils/http.ts
+++ src/scene/admin/vue3-admin/src/utils/http.ts
@@ -1,3 +1,6 @@
 import axios from 'axios'
-const http = axios
+import { API_PREFIX } from '@/config'
+const http = axios.create({
+  baseURL: API_PREFIX
+})
 export default http
